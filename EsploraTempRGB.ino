33d15e0f41292475be7fb09ad92c9869b341925c
javacasm <javacasm@gmail.com>
2015-01-13 14:15:53 +0100

    3 Version

--- EsploraTempRGB.ino
+++ EsploraTempRGB.ino
@@ -1,6 +1,6 @@
 /*
-  Show the temperature, in the RGB LED using a Esplora Arduino Board
-  Based in  Esplora Temperature Sensor example by Tom Igoe
+ Show the temperature, in the RGB LED using a Esplora Arduino Board
+ Based in  Esplora Temperature Sensor example by Tom Igoe
 
  Arduino Esplora uses a TMP36 sensor for reading the temperature.
  Its appears to depend heavily from the input voltage
